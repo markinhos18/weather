f700693bfcf4aea406aad2de1147f867d47dbcef
markinhos18 <marcosfelipe@mfwebdeveloper.net.br>
2023-05-16 21:34:17 -0300

    weather page

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+// import './styles.css';
+
+// const GEOCODE_API_KEY = 'YOUR_GEOCODE_API_KEY'; // Replace with your Geocode API key
+// const OPENMETEO_API_KEY = 'YOUR_OPENMETEO_API_KEY'; // Replace with your OpenMeteo API key
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [address, setAddress] = useState('');
+
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      const response = await axios.get(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  const fetchCoordinates = async () => {
+    try {
+      const response = await axios.get(
+        `https://geocode.maps.co/search?q=${address}`
+      );
+      const { latitude, longitude } = response.data.results[0].geometry;
+      fetchWeatherData(latitude, longitude);
+    } catch (error) {
+      console.error('Error fetching coordinates:', error);
+    }
+  };
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchCoordinates();
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherData && (
+        <div className="weather-container">
+          <h2>{weatherData.city.name}</h2>
+          <p>{weatherData.current_condition.temperature}°C</p>
+          <p>{weatherData.current_condition.weather}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
